f87300b12be4ec36f572ae0112cccdbde468705e
johnny20061234 <46470547+congaterori@users.noreply.github.com>
2020-01-26 18:05:51 -0500

    rocket Version 1.0
    file batch file rocket

---
+++ rocketSP.bat
@@ -0,0 +1,91 @@
+@echo off
+color a
+title rocket SP
+cls
+type rocketfl.txt
+timeout 5 > nul
+cls
+echo -18:05
+type rocketrun.txt
+timeout 20 > nul
+cls
+type rocket2.txt
+timeout 3 > nul
+cls
+type rocket3.txt
+timeout 4 > nul
+cls
+type rocket4.txt
+timeout 2 > nul
+cls
+type rocket5.txt
+timeout 3 > nul
+cls
+type rocket6.txt
+timeout 5 > nul
+cls
+type rocket7.txt
+timeout 2 > nul
+cls
+type rocket8.txt
+timeout 2 > nul
+cls
+type rocket9.txt
+timeout 2 > nul
+cls
+type rocket10.txt
+timeout 10 > nul
+cls
+type rocket11.txt
+timeout 1 > nul
+cls
+type rocket12.txt
+timeout 1 > nul
+cls
+type rocket13.txt
+timeout 2 > nul
+cls
+type rocket14.txt
+timeout 5 > nul
+cls
+type rocket15.txt
+timeout 2 > nul
+cls
+type nhaydu.txt
+timeout 5 > nul
+cls
+type rocket16.txt
+timeout 4 > nul
+cls
+type rocket17.txt
+timeout 1 > nul
+cls
+type rocket18.txt
+timeout 1 > nul
+cls
+type rocket19.txt
+timeour 1 > nul
+cls
+type rocket 20.txt
+timeout 1 > nul
+cls
+echo connect -ship1
+timeout 3 > nul
+cls
+type ship1.txt
+pause > nul
+cls
+type trusochinh.txt
+pause > nul
+cls
+type tramnghiengcuu.txt
+pause > nul
+cls
+type save.txt
+echo.
+type tauappo12.txt
+pause > nul
+type appolo.txt
+echo.
+pause
+exit
